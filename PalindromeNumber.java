465d657e2e0b8137ab649d4751a9b3489b1091ad
abdellah ait bachikh <abdo.jhon2023@gmail.com>
2025-08-19 14:16:59 +0100

    isPalindrome

---
+++ PalindromeNumber.java
@@ -0,0 +1,18 @@
+class PolindromBumber {
+
+     static public boolean isPalindrome(int x) {
+        String stringNumber = Integer.toString(x);
+        for (int i = 0, j = stringNumber.length() - 1; i < j; i++, j--) {
+            if (stringNumber.charAt(i) != stringNumber.charAt(j)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    public static void main(String[] args){
+
+        System.out.print(isPalindrome(123));
+    }
+ 
+}
