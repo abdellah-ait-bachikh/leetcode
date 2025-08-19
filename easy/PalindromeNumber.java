cbbf0649e5b4e0309552d4dc3e5bfa3883ba7b67
abdellah ait bachikh <abdo.jhon2023@gmail.com>
2025-08-19 14:18:37 +0100

    move t easy

--- easy/PalindromeNumber.java
+++ easy/PalindromeNumber.java
@@ -1,6 +1,7 @@
+package easy;
 class PolindromBumber {
 
-     static public boolean isPalindrome(int x) {
+    static public boolean isPalindrome(int x) {
         String stringNumber = Integer.toString(x);
         for (int i = 0, j = stringNumber.length() - 1; i < j; i++, j--) {
             if (stringNumber.charAt(i) != stringNumber.charAt(j)) {
@@ -10,9 +11,10 @@ class PolindromBumber {
 
         return true;
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
 
         System.out.print(isPalindrome(123));
     }
- 
+
 }
