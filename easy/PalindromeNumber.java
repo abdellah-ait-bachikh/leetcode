171ad23f211405647d3a21a8dfe0eaa6a895d718
abdellah ait bachikh <abdo.jhon2023@gmail.com>
2025-08-19 14:21:29 +0100

    clean code

--- easy/PalindromeNumber.java
+++ easy/PalindromeNumber.java
@@ -1,10 +1,11 @@
 package easy;
+
 class PolindromBumber {
 
     static public boolean isPalindrome(int x) {
-        String stringNumber = Integer.toString(x);
-        for (int i = 0, j = stringNumber.length() - 1; i < j; i++, j--) {
-            if (stringNumber.charAt(i) != stringNumber.charAt(j)) {
+        for (int i = 0, j = Integer.toString(x).length() - 1; i < j; i++, j--) {
+            if (Integer.toString(x).charAt(i) != Integer.toString(x).charAt(j)) {
+
                 return false;
             }
         }
@@ -14,7 +15,7 @@ class PolindromBumber {
 
     public static void main(String[] args) {
 
-        System.out.print(isPalindrome(123));
+        System.out.print(isPalindrome(12321));
     }
 
 }
